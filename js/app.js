65248e8a822da25dae1e540a691232fa4715bfee
ALEX <jager@templatemonster.me>
2016-07-13 12:53:44 +0000

    перенос функционала в отдельный файл, рефакторинг

--- js/app.js
+++ js/app.js
@@ -1,119 +1,31 @@
 var app = angular.module('calc', []);
-var storage = undefined;
-var Model = function(){
-	this.DIGIT1 = "1";
-	this.DIGIT2 = "2";
-	this.DIGIT3 = "3";
-	this.DIGIT4 = "4";
-	this.DIGIT5 = "5";
-	this.DIGIT6 = "6";
-	this.DIGIT7 = "7";
-	this.DIGIT8 = "8";
-	this.DIGIT9 = "9";
-	this.DIGIT0 = "0";
-	this.OP_DIV = "/";
-	this.OP_MUL = "*";
-	this.OP_MNS = "-";
-	this.OP_PLS = "+";
-	this.OP_PNT = ".";
-	this.OP_LBR = "(";
-	this.OP_RBR = ")";
-	this.OP_BCK = "←";
-	this.OP_CLR = "C";
-	this.OP_EQL = "=";
-	this.OP_PST = "%";
-	this.OP_SQT = "√";
-	this.OP_SQR = "x²";
-	this.OP_INV = "+-";
-	this.OP_MMS = "M-";
-	this.OP_MPL = "M+";
-	this.OP_MCL = "MC";
-	this.OP_MRD = "MR";
-	
-	var _this = this;
-	var keys = [
-		{label: this.DIGIT1, value: 1}, 
-		{label: this.DIGIT2, value: 2}, 
-		{label: this.DIGIT3, value: 3},
-		{label: this.DIGIT4, value: 4}, 
-		{label: this.DIGIT5, value: 5}, 
-		{label: this.DIGIT6, value: 6},
-		{label: this.DIGIT7, value: 7}, 
-		{label: this.DIGIT8, value: 8}, 
-		{label: this.DIGIT9, value: 9},
-		{label: this.DIGIT0, value: 0},
-		{label: this.OP_PNT, value: "."}, 
-		{label: this.OP_DIV, operation: "/"}, 
-		{label: this.OP_MUL, operation: "*"},
-		{label: this.OP_MNS, operation: "-"}, 
-		{label: this.OP_PLS, operation: "+"},
-		{label: this.OP_LBR, operation: "("},
-		{label: this.OP_RBR, operation: ")"}, 
-		{label: this.OP_BCK, operation: "back"},
-		{label: this.OP_CLR, operation: "clear"}, 
-		{label: this.OP_EQL, operation: "equal"},
-		{label: this.OP_PST, operation: function(a){ return a / 100 }},
-		{label: this.OP_SQT, operation: function(a){ return Math.sqrt(a) }},
-		{label: this.OP_SQR, operation: function(a){ return Math.pow(a, 2) }},
-		{label: this.OP_INV, operation: function(a){ 
-			if(a == '') throw new Error('empty');
-			return (parseFloat(a) * -1)+'' 
-		}},
-		{label: this.OP_MMS, operation: function(a){ 
-			if(storage != undefined){
-				storage -= parseFloat(a) ;
-			}else {
-				storage = parseFloat(a) * -1;
-			}
-			
-			return storage; 
-		}},
-		{label: this.OP_MPL, operation: function(a){ 
-			if(storage != undefined){
-				storage += parseFloat(a) ;
-			}else {
-				storage = parseFloat(a) ;
-			}
-			
-			return storage; 
-		}},
-		{label: this.OP_MCL, operation: function(){ 
-			storage = 0;
-			
-			return storage;
-		}},
-		{label: this.OP_MRD, operation: function(){ 
-			return storage;
-		}}
-	];
-	
-	this.getKey =function(keyName){
-		for(var key in keys){
-			if(keys[key].label == keyName.toString()) {
-				return keys[key];
-			}
-		}
-		
-		return null;
-	}
-	
-	this.getKeys = function(_keys){
-		var keys_arr = [];
-		for(var key in _keys){
-			var k = _this.getKey(_keys[key]);
-			if(k){
-				keys_arr.push(k);
-			}
-		}
-		
-		return keys_arr;
-	}
-}
-
-var data_model = new Model();
-
 app.controller("calcController", function ($scope) {
-	$scope.keyClicked = function(key) {
+	//размеры
+	$scope.type = calc.SIMPLE;
+	$scope.result = '0';
+	
+	$scope.setType = function(type) {
+		$scope.type = type;
+		$scope.keys = calc.setType($scope.type);
+	}
+	$scope.keyClicked = function(key){
+		var res = calc.enterKey(key);
+		$scope.result = resToStr(res);
+		setSize();
+	}
+	
+	function setSize() {
+		$scope.middle_size = resToStr($scope.result).length > 16;
+		$scope.small_size = resToStr($scope.result).length > 21;
+	}
+	function resToStr(res) {
+		return res + '';
+	}
+});
+
+/*
+
+$scope.keyClicked = function(key) {
 		if(key.value !== undefined) {
 			$scope.digitClicked(key.value)
 		} else if (key.operation !== undefined) {
@@ -134,8 +46,6 @@ app.controller("calcController", function ($scope) {
 		}
 		
 		clear_first_zero($scope.display_history);
-		$scope.small_size = $scope.display_history.length > 16;
-		$scope.mikro_size = $scope.display_history.length > 21;
 	}
 	
 	$scope.operationClicked = function (key) {
@@ -278,16 +188,4 @@ app.controller("calcController", function ($scope) {
 			
 		return val;
 	}
-	
-	$scope.small_size = false;
-	$scope.mikro_size = false;
-	$scope.type = 'simple';
-	$scope.enterDigit = true;
-	$scope.display_history = '0';
-	$scope.displayValue = 0;
-	$scope.valueA = '';
-	$scope.clearValue = true;
-	$scope.clearHistory = true;
-	
-	$scope.setType($scope.type);
-});
+*/
