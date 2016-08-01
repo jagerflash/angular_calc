dbfcf3b5bc6c3c078d650fdb5e378e7b7dad4aa8
jagerflash <jager.flash@gmail.com>
2016-08-01 03:08:15 +0000

    добавил тестов, описание

--- tests/spec.js
+++ tests/spec.js
@@ -50,6 +50,7 @@ describe('calculator', function () {
       expect($calcService).toBeDefined();
     });
   });
+  
   describe('model-test', function () {
     it('has all keys', function(){
       var havnt_key = false;
@@ -60,6 +61,70 @@ describe('calculator', function () {
         }
       }
       expect(havnt_key).toBeFalsy();
+    });
+    it('has a storage', function() {
+        expect($dataService.getStorage()).toBeDefined();
     })
+  });
+  
+  describe('calc-test', function () {
+    function enterKey(key) {
+      return $calcService.enterKey($dataService.getKey(key));
+    }
+    function clear_display() {
+      $calcService.enterKey($dataService.getKey($keysService.OP_CLR));
+    }
+    it('cancel key is work', function (){
+      expect($calcService.enterKey($dataService.getKey($keysService.OP_CLR)).result).toBe(0);
+    });
+    it('digit keys works', function () {
+      expect(enterKey($keysService.DIGIT0).result).toBe('0');
+      clear_display();
+      expect(enterKey($keysService.DIGIT1).result).toBe('1');
+      clear_display();
+      expect(enterKey($keysService.DIGIT2).result).toBe('2');
+      clear_display();
+      expect(enterKey($keysService.DIGIT3).result).toBe('3');
+      clear_display();
+      expect(enterKey($keysService.DIGIT4).result).toBe('4');
+      clear_display();
+      expect(enterKey($keysService.DIGIT5).result).toBe('5');
+      clear_display();
+      expect(enterKey($keysService.DIGIT6).result).toBe('6');
+      clear_display();
+      expect(enterKey($keysService.DIGIT7).result).toBe('7');
+      clear_display();
+      expect(enterKey($keysService.DIGIT8).result).toBe('8');
+      clear_display();
+      expect(enterKey($keysService.DIGIT9).result).toBe('9');
+      clear_display();
+    });
+    it('sum is good', function () {
+      enterKey($keysService.DIGIT1);
+      enterKey($keysService.OP_PLS);
+      enterKey($keysService.DIGIT1);
+      
+      expect(enterKey($keysService.OP_EQL).result).toBe(2);
+    });
+    it('mulpty point test', function () {
+      enterKey($keysService.DIGIT1);
+      enterKey($keysService.OP_PNT);
+      enterKey($keysService.OP_PNT);
+      
+      expect(enterKey($keysService.DIGIT1).result).toBe('1.1');
+    });
+    it('mulpty operation test', function () {
+      enterKey($keysService.DIGIT1);
+      enterKey($keysService.OP_MUL);
+      enterKey($keysService.OP_DIV);
+      
+      expect(enterKey($keysService.DIGIT1).result).toBe('1/1');
+    });
+    it('backspace operation test', function () {
+      enterKey($keysService.DIGIT1);
+      enterKey($keysService.DIGIT0);
+      
+      expect(enterKey($keysService.OP_BCK).result).toBe('1');
+    });
   });
 });
